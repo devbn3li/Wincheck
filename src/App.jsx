de167a672b44e0369bf7d688a0eefd85560269e3
FaisalIbrahim404 <ff9371668@gmail.com>
2024-12-01 14:31:59 +0200

    [enhance]

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Button } from "antd";
+import React, { useEffect } from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    useNavigate,
+} from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
 import Map from "./Map";
-import create from "zustand";
 import Cookies from "js-cookie";
 
-// Zustand store
-const useStore = create((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-}));
+const App = () => {
+    const navigate = useNavigate();
+    useEffect(() => {
+        const token = Cookies.get("session_token");
+        if (!token) {
+            navigate("/login");
+        }
+    }, [navigate]);
 
-export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Map />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Routes>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Map />} />
+        </Routes>
+    );
 };
+
+export default function AppWrapper() {
+    return (
+        <Router>
+            <App />
+        </Router>
+    );
+}
